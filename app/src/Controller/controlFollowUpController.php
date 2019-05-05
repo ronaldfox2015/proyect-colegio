edc9a8969e24920524c8abc68cbab4b3697a141a
Ronald Cutisaca <ronaldfox2015@gmail.com>
2019-05-05 08:15:14 -0500

    wip

--- app/src/Controller/controlFollowUpController.php
+++ app/src/Controller/controlFollowUpController.php
@@ -7,9 +7,10 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class controlFollowUpController extends AppController
 {
+    public $activate;
     public function __construct()
     {
-
+        $this->activate = '';
     }
 
     /**
@@ -19,7 +20,19 @@ class controlFollowUpController extends AppController
      */
     public function index()
     {
-        return $this->renderApp('control-follow-up/index.html.twig',[]);
+//        $ch = curl_init();
+//
+//        curl_setopt($ch, CURLOPT_HEADER, 0);
+//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //Set curl to return the data instead of printing it to the browser.
+//        curl_setopt($ch, CURLOPT_URL, "https://api.aptitus.com/locations?q=peru");
+//
+//        $data = curl_exec($ch);
+//        curl_close($ch);
+//
+//        var_dump($data);exit;
+        return $this->renderApp('control-follow-up/index.html.twig',[
+            'activate' => 'active'
+        ]);
     }
 
     /**
@@ -29,6 +42,8 @@ class controlFollowUpController extends AppController
      */
     public function editReport()
     {
-        return $this->renderApp('control-follow-up/edit-report.html.twig',[]);
+        return $this->renderApp('control-follow-up/edit-report.html.twig',[
+            'activate' => 'active'
+        ]);
     }
 }
